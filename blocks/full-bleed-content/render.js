557076977a48753e5a452dc5fc2b587e7c475dc7
Uriel Arana Robles <uarana@MXSCHLC-UARANA.local>
2026-02-10 12:57:09 -0600

    feat(component): full-bleed-content js structure and styles was added

---
+++ blocks/full-bleed-content/render.js
@@ -0,0 +1,34 @@
+import { h } from '@dropins/tools/preact.js';
+import htm from '../../scripts/htm.js';
+
+const html = htm.bind(h);
+
+export default function FullBleedContent({
+    imgDesk,
+    imgMob,
+    background,
+    alt,
+    title,
+    description,
+    href,
+    leftAlign }) {
+
+    const bleed = html`
+        <section className="full-bleed-content" style=${{ backgroundColor: background }}>
+            <div className="full-bleed-content_father-container" style=${leftAlign?{flexDirection: 'row-reverse'}:{flexDirection: 'row'}}>
+                <div className="full-bleed-content_text-container">
+                    <h2>${title}</h2>
+                    <p>${description}</p>
+                </div>
+                <div className="full-bleed-content_image-container">
+                    <a href=${href}>
+                        <img className="full-bleed-content_image-desktop" src=${imgDesk} alt=${alt} />
+                        <img className="full-bleed-content_image-mobile" src=${imgMob} alt=${alt} />
+                    </a>
+                </div>
+            </div>
+        </section>
+        `;
+    console.log("Mensaje desde el render jeje")
+    return bleed;
+}
